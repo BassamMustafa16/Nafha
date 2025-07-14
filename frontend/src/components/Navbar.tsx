4bef432fcee385d81e77187f3dd1f6de76df12a6
BassamMustafa16 <basam_wadi@yahoo.com>
2025-07-14 22:01:44 +0300

    Design improvments

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   const [showNavs, setShowNavs] = useState(false);
 
   return (
-    <nav className="relative md:absolute top-0 left-0 flex flex-col md:flex-row w-full  xl:gap-10 items-center justify-center lg:text-xl bg-transparent text-black md:text-white z-10 py-5">
+    <nav className="md:absolute top-0 left-0 flex flex-col md:flex-row w-full  xl:gap-10 items-center justify-center lg:text-xl bg-transparent text-black md:text-white z-10 py-5 md:py-0">
       {/* Mobile Nav */}
       <div className="flex flex-row justify-between px-4 items-center w-full md:hidden">
         <Link href="/" className="w-1/2">
@@ -27,11 +27,11 @@ export default function Navbar() {
 
       {/* Rolling collapse wrapper */}
       <div
-        className={`overflow-hidden transition-all duration-500 ease-in-out origin-top transform 
+        className={`overflow-hidden transition-all duration-500 ease-in-out origin-top transform
           ${showNavs ? "max-h-[500px]" : "max-h-0"}
           md:max-h-none`}
       >
-        <div className="flex flex-col gap-5 items-center py-5">
+        <div className="flex flex-col md:flex-row gap-5 items-center py-5 md:py-0">
           <Link href="/" className="hover:font-semibold">
             Home
           </Link>

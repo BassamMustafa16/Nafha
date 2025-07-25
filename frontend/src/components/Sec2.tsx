57e42775b10ed8c6f4368a96ad13ccec53a5b3e4
BassamMustafa16 <basam_wadi@yahoo.com>
2025-07-25 15:45:39 +0400

    Homepage sec 2 image shift issue

--- frontend/src/components/Sec2.tsx
+++ frontend/src/components/Sec2.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Sec2() {
   return (
-    <section className="relative h-screen mt-10">
+    <section className="relative min-h-[100svh] md:min-h-screen mt-10">
       {/* Sticky Parallax Image */}
-      <div className="relative w-full h-screen">
+      <div className="relative w-full min-h-[100svh] md:min-h-screen">
         <Image
           src="/images/sec 02.png"
           alt="sec 02"
@@ -14,7 +14,6 @@ export default function Sec2() {
           className="object-cover object-center"
         />
       </div>
-
       <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-full flex flex-col gap-10 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 z-10 items-center text-white">
         <h2 className="text-xl md:text-2xl lg:text-4xl text-center font-light tracking-wide">
           DISCOVER CREATIONS THAT SPEAK POWER, <br />
